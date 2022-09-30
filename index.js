3d29b8465b25c579fbf6f6734c9988120d81bd21
William Gadd <wgadd@sasktel.net>
2022-09-30 16:07:52 +0000

    Started refactoring arrays of values into an array of objects

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-// Arrays to keep track of each task's state
+/* // Arrays to keep track of each task's state
 const taskTitles = [];
 const taskComplete = [];
-
-// Create a new task by adding to the arrays
+const taskDescriptions = [];
+ */// Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
-}
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
+/* // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
 function completeTask(taskIndex) {
   taskComplete[taskIndex] = true;
 }
@@ -20,12 +16,27 @@ function logTaskState(taskIndex) {
   const complete = taskComplete[taskIndex];
   console.log(`${title} has${complete ? " " : " not "}been completed`);
 }
+ */
+
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+  };
+
+  return task;
+}
 
 // DRIVER CODE BELOW
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+const task1 = newTask("Clean Cat Litter", "Take all the \u{1F4A9} out of the litter box"); // task 0
+const task2 = newTask("Do Laundry", "\u{1F628}");
+const tasks = [task1, task2];
 
-logTaskState(0); // Clean Cat Litter has not been completed
+console.log(tasks);
+
+/* logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
 logTaskState(0); // Clean Cat Litter has been completed
+ */

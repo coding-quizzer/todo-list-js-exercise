f710552cd6128e1e74f3855ea3da6f7d56362298
William Gadd <wgadd@sasktel.net>
2022-09-30 16:19:56 +0000

    Removed redundant functions

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/* // Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
-const taskDescriptions = [];
- */// Create a new task by adding to the arrays
-// A new task will be created as incomplete
-
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
 function completeTask(task) {
   task.complete = true;
